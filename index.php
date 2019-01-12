ec9ade0e3240e37441ff54f2502caaf1a9fac2ac
david <david.dmello@hnrtech.com>
2019-01-12 10:31:28 +0530

    added font awsome icon

--- index.php
+++ index.php
@@ -28,6 +28,10 @@ $rows_dealer_info = $DBI->get_result($select_dealer_info);
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <!-- Bootstrap 3.3.5 -->
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <!-- Ionicons -->
+        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
         <!-- Theme style -->
         <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
         <!-- AdminLTE Skins. Choose a skin from the css/skins
--- sidebar.php
+++ sidebar.php
@@ -7,15 +7,31 @@
             <?php
               if($_SESSION['role'] == 'superadmin'){
             ?>
+
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-bell-o"></i> <span>Test</span>
+                    <span class="pull-right-container">
+                      <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li class=""><a href="{{ url('my-reminders') }}"><i class="fa fa-bell-o"></i>Test 1</a></li>
+                    <li class="">
+                        <a href="{!! route('reminder-settings', ['id'=> Auth::user()->id]) !!}"><i class='fa fa-cogs'></i> <span>Test 2</span></a>
+                    </li>
+                </ul>
+            </li>
+
 			      <li>
               <a href="index.php">
-                <i class="fa fa-th"></i> <span>Dealer</span>
+                <i class="fa fa-user"></i> <span>Dealer</span>
               </a>
             </li>
 
             <li>
               <a href="view_brand_model.php">
-                <i class="fa fa-th"></i> <span>Brand Model Master</span>
+                <i class="fa fa-btc"></i> <span>Brand Model Master</span>
               </a>
             </li>
 
@@ -27,43 +43,43 @@
 
             <li>
               <a href="view_insurance_company.php">
-                <i class="fa fa-th"></i> <span>Insurance Company Master</span>
+                <i class="fa fa-building"></i> <span>Insurance Company Master</span>
               </a>
             </li>
 
             <li>
               <a href="view_shop_amenities.php">
-                <i class="fa fa-th"></i> <span>Shop Amenities Master</span>
+                <i class="fa fa-shopping-cart"></i> <span>Shop Amenities Master</span>
               </a>
             </li>
 
             <li>
               <a href="view_shop_service.php">
-                <i class="fa fa-th"></i> <span>Shop Service Master</span>
+                <i class="fa fa-bullhorn"></i> <span>Shop Service Master</span>
               </a>
             </li>
 
             <li>
               <a href="view_services_repair.php">
-                <i class="fa fa-th"></i> <span>Services Repair Master</span>
+                <i class="fa fa-wrench"></i> <span>Services Repair Master</span>
               </a>
             </li>
 
             <li>
               <a href="view_cust_feedback.php">
-                <i class="fa fa-th"></i> <span>Custmore Feedback</span>
+                <i class="fa fa-bullhorn"></i> <span>Custmore Feedback</span>
               </a>
             </li>
 
             <li>
               <a href="view_contact_us.php">
-                <i class="fa fa-th"></i> <span>Custmore Contact Us</span>
+                <i class="fa fa-list-alt"></i> <span>Custmore Contact Us</span>
               </a>
             </li>
 
             <li>
               <a href="view_add_home_page_images.php">
-                <i class="fa fa-th"></i> <span>Home Page Images</span>
+                <i class="fa fa-file-image-o"></i> <span>Home Page Images</span>
               </a>
             </li>
             <?php
@@ -72,7 +88,7 @@
             ?>
             <li>
               <a href="appointments.php">
-                <i class="fa fa-th"></i> <span>Appointment</span>
+                <i class="fa fa-calendar"></i> <span>Appointment</span>
               </a>
             </li>
             <?php
@@ -80,7 +96,7 @@
             ?>
             <li>
               <a href="change_password.php">
-                <i class="fa fa-th"></i> <span>Changed Password</span>
+                <i class="fa fa-key"></i> <span>Changed Password</span>
               </a>
             </li>
           </ul>
